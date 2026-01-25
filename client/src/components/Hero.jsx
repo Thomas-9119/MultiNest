dff1166bdd27a38b5e3c78cb0162c3303093c581
Thomas-9119 <dharrajparno@gmail.com>
2026-01-25 14:47:52 +0530

    Commit

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-blue-100 bg-cover bg-no-repeat min-h-screen'>
+      <div  className='text-center mb-6'>
+        <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl
+        font-semibold mx-auto leading-[1.2]'>Creating Amazing Projects <br />using <span className='text-primary'>AI tools</span></h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas laborum velit debitis laboriosam porro animi distinctio omnis nemo vitae recusandae?</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
